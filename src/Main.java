cd41ade29604b87ce6365973d5f989286e918cd6
Richard Flores <lcdo.richardflores@gmail.com>
2022-09-27 00:45:07 -0300

    Primera parte del ejercicio, crear función

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(5,8,10);
+        System.out.println(resultado);
+    }
+    public static int suma (int a, int b, int c) {
+        return a + b + c;
+    }
+}
